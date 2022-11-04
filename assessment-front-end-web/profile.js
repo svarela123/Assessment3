827611a4632bddfd76534dcff8a0955336e359db
Sergio Varela <116034631+svarela123@users.noreply.github.com>
2022-11-04 12:43:18 -0600

    Add files via upload

---
+++ assessment-front-end-web/profile.js
@@ -0,0 +1,17 @@
+let btnFavColor = document.querySelector('#color');
+
+btnFavColor.addEventListener('click', () => {
+	alert('My favorite color is grey!');
+  });
+
+  let btnFavPlace = document.querySelector('#place');
+
+btnFavPlace.addEventListener('click', () => {
+	alert('My favorite place is home!');
+  });
+
+  let btnFavRitual = document.querySelector('#ritual');
+
+btnFavRitual.addEventListener('click', () => {
+	alert('My favorite ritual is gathering with family for the holidays!');
+  });
